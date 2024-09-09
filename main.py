52dee13e124072b8230ce7d987770377b49c1375
jonas-b <bauer.jonas@outlook.com>
2024-09-09 16:45:52 +0200

    Added player interactivety

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
 
 def main():
     pygame.init()
+    game_time = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        updatable.update(dt)
         screen.fill((0, 0, 0))
+        drawable.draw(screen)
+        dt = game_time.tick(60) / 1000
         pygame.display.flip()
     pygame.quit()
 
